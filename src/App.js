6c9843a5935399a364f6c85fe48ce6e1e3a99fca
Adam Oswalt <email@adamoswalt.com>
2017-04-02 12:36:10 -0500

    function to get length of longest participant name

--- src/App.js
+++ src/App.js
@@ -27,10 +27,21 @@ const bracket = [
   ],
 ]
 
+const longestEntry = (list, lengthFunc, initalMax = 0) =>
+  list.reduce((max, l) => Math.max(max, lengthFunc(l)), initalMax)
+
+const stringLength = (s) => s.length
+
+const longestEntryLength = (l, initalMax = 0) => longestEntry(l, stringLength, initalMax)
+
+const getLongestParticipantLength = (roundList) =>
+  roundList.reduce((max, round) => longestEntry(round, longestEntryLength, max), 0)
+
 /** Main app component. */
 class App extends PureComponent {
   /** Render method. */
   render() {
+    console.warn(getLongestParticipantLength(bracket))
     return (
       <Bracket bracket={bracket} />
     )
