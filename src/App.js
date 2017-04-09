c54d39d09980d88ecb9ccd1415a0e7d3b08ccc58
Adam Oswalt <email@adamoswalt.com>
2017-04-08 21:33:37 -0500

    working on encoded and parsing match ids

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { PureComponent } from 'react'
 import { ArrayX } from './util'
 import Bracket from './components/Bracket'
+import makeStruct from './util/structor'
 
 const participantList = [
   'lorem',
@@ -64,7 +65,7 @@ const mapping = {
     l: null,
   },
   '0-0-3': {
-    w: { b: 0, r: 1, m: 1, p: 1 },
+    w: { b: 0, r: 2, m: 1, p: 1 },
     l: null,
   },
   '0-1-0': {
@@ -81,8 +82,13 @@ const mapping = {
   },
 }
 
-const buildMatches = ({ head, tail }, acc = []) =>
-  tail.length ? buildMatches(ArrayX.take(tail, 2), [...acc, head]) : [...acc, head]
+
+const buildMatches = ({ head, tail }, acc = []) => {
+  const currentMatchList = [...acc, head]
+  return tail.length
+    ? buildMatches(ArrayX.take(tail, 2), currentMatchList)
+    : currentMatchList
+}
 
 const buildRound = list => buildMatches(ArrayX.take(list, 2))
 
@@ -98,10 +104,45 @@ const buildBracket = (participantList) => {
   return finishRounds([initialMatches])
 }
 
+
+const Match = makeStruct('bracket round match position')
+const parseMatchId = (matchId) => {
+  const idParts = matchId.split('-')
+  const matchArgs = idParts.map(i => { const val = parseInt(i); return val === NaN ? undefined : val })
+  return new Match(...matchArgs)
+}
+const encodeMatchId = (m) => `${m.bracket}-${m.round}-${m.match}-${m.position}`
+
+const encodedMapping = [
+  '0-0-0 0-1-0-0 _',
+  '0-0-1 0-1-0-1 _',
+  '0-0-2 0-1-1-0 _',
+  '0-0-3 0-2-1-1 _',
+  '0-1-0 0-2-0-0 _',
+  '0-1-1 0-2-0-1 _',
+  '0-2-1 _ _',
+]
+
+const parsedMapping = encodedMapping.reduce((acc, m) => {
+  const data = m.split(' ')
+  return {
+    ...acc,
+    [data[0]]: {
+      win: parseMatchId(data[1]),
+      lose: parseMatchId(data[2]),
+    }
+  }
+}, {})
+
+
 /** Main app component. */
 class App extends PureComponent {
   /** Render method. */
   render() {
+    console.log(encodedMapping)
+    console.log(parsedMapping)
+    console.warn(encodeMatchId(parseMatchId(encodeMatchId(parseMatchId(encodeMatchId(parseMatchId('_')))))))
+
     return (
       <div>
         <Bracket bracket={buildBracket(participantList)} />
